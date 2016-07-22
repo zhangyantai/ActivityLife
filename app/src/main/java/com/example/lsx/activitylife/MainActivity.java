455f88a5507c43405c7647970f7b965a956795f8
zhangyantai <zhangyantai@github.com>
2016-07-22 15:18:52 +0800

    add ActivityLife app

---
+++ app/src/main/java/com/example/lsx/activitylife/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.lsx.activitylife;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+   private final static String TAG="MainActivity";
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    public void click(View v)
+    {
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+  
+}
---
+++ app/src/main/java/com/example/lsx/activitylife/SecondActivity.java
@@ -0,0 +1,9 @@
+package com.example.lsx.activitylife;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class SecondActivity extends AppCompatActivity {
+}
